e9ed9ce86485c1255e9fdc56cf7e6bd5ae46fa21
Cezar Augusto Mezzalira <cezar.mezzalira@docente.pr.senac.br>
2024-11-27 21:29:58 -0300

    resolução exercicios

--- src/exercicio-03.ts
+++ src/exercicio-03.ts
@@ -3,3 +3,21 @@
 // infantil (até 13 anos),
 // juvenil (até 17 anos) ou
 // sênior (acima de 17 anos).
+
+// ENTRADA
+const idadeJogador = 12;
+
+// PROCESSAMENTO
+
+let categoria = "";
+
+if (idadeJogador <= 13) {
+  categoria = "Fraldinha";
+} else if (idadeJogador <= 17) {
+  categoria = "Juvenil";
+} else {
+  categoria = "Bolero";
+}
+
+// SAÍDA
+console.log("Categoria indicada: " + categoria);
--- src/exercicio-04.ts
+++ src/exercicio-04.ts
@@ -1,2 +1,55 @@
 // 4 - Escreva um programa que leia três números do teclado
 // e os imprima na tela na ordem inversa da entrada.
+
+// while - enquanto
+let repeticoes = 1;
+while (repeticoes < 10) {
+  console.log("Sou eu de novo");
+  // repeticoes = repeticoes + 1;
+  repeticoes++; //autoincremento
+}
+
+// do/while - faca/enquanto
+repeticoes = 10;
+
+console.log("DO/WHILE");
+
+do {
+  console.log("Sou eu de novo +" + repeticoes);
+  repeticoes--; //autodecremento
+} while (repeticoes > 0);
+
+// validação de senhas
+const senha = "senhasegura";
+let tentativas = 0;
+let senhaValida = false;
+const senhas = ["senhasegura", "nadaaver", "senhasegura"];
+
+do {
+  if (senhas[tentativas] === senha) {
+    senhaValida = true;
+    console.log("Acertou bicho!");
+  } else {
+    console.log("a senha pertence ao fulano");
+  }
+  tentativas++;
+} while (tentativas < 3 && !senhaValida);
+
+const numeros: number[] = [10, 4, 22];
+// for - para
+for (let indice = 0; indice < numeros.length; indice++) {
+  console.log(numeros[indice]);
+}
+
+let indice = numeros.length - 1;
+while (indice >= 0) {
+  console.log(numeros[indice]);
+  indice = indice - 1;
+}
+
+for (let i = numeros.length - 1; i >= 0; i--) {
+  console.log(numeros[i]);
+}
+
+numeros.reverse();
+console.log(numeros);
